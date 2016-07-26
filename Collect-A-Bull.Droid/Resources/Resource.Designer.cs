810d980b0ccf9f143dd5b6a2b8cfb3af8ab82651
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-07-26 16:28:43 +0500

    ListView

--- Collect-A-Bull.Droid/Resources/Resource.Designer.cs
+++ Collect-A-Bull.Droid/Resources/Resource.Designer.cs
@@ -118,8 +118,8 @@ namespace Collect_A_Bull.Droid
 			// aapt resource value: 0x7f050006
 			public const int button1 = 2131034118;
 			
-			// aapt resource value: 0x7f050007
-			public const int button2 = 2131034119;
+			// aapt resource value: 0x7f05000e
+			public const int button2 = 2131034126;
 			
 			// aapt resource value: 0x7f050003
 			public const int editText1 = 2131034115;
@@ -127,11 +127,38 @@ namespace Collect_A_Bull.Droid
 			// aapt resource value: 0x7f050005
 			public const int editText2 = 2131034117;
 			
+			// aapt resource value: 0x7f05000f
+			public const int linearLayout1 = 2131034127;
+			
+			// aapt resource value: 0x7f050010
+			public const int listView1 = 2131034128;
+			
 			// aapt resource value: 0x7f050002
 			public const int textView1 = 2131034114;
 			
 			// aapt resource value: 0x7f050004
 			public const int textView2 = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView3 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView4 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView5 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int textView6 = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView7 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView8 = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int textView9 = 2131034125;
 			
 			static Id()
 			{
@@ -150,13 +177,16 @@ namespace Collect_A_Bull.Droid
 			public const int AddView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int HomeView = 2130903041;
+			public const int CollectableItem = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int ListView = 2130903042;
+			public const int HomeView = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int SplashScreen = 2130903043;
+			public const int ListView = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int SplashScreen = 2130903044;
 			
 			static Layout()
 			{
