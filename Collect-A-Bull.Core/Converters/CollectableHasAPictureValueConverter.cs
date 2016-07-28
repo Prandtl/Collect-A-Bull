4527f4be875803f512095b1a56187ecd0b120ce0
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-07-28 12:11:37 +0500

    added value converters for future use

---
+++ Collect-A-Bull.Core/Converters/CollectableHasAPictureValueConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Collect_A_Bull.Core.Services.DataStore;
+using MvvmCross.Platform.Converters;
+
+namespace Collect_A_Bull.Core.Converters
+{
+	public class CollectableHasAPictureValueConverter:MvxValueConverter<Collectable,bool>
+	{
+		protected override bool Convert(Collectable collectable, Type targetType, object parameter, CultureInfo culture)
+		{
+			var result = collectable?.ImagePath != null;
+			return result;
+		}
+	}
+}
